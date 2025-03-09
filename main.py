93eb305a811a8d655f21be274dc94cff187e5512
Richard Cross <97173980+Trickaay@users.noreply.github.com>
2025-03-09 00:10:39 +0000

    Create grid.py
    Added the game grid

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-import pygame
-
-import sys
+import pygame, sys
+from grid import Grid
 
 pygame.init()
 dark_blue = (44, 44, 127)
 
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((300, 600))
 pygame.display.set_caption("Python Tetris")
 
 clock = pygame.time.Clock()
+
+game_grid = Grid()
+game_grid.print_grid()
 
 while True:
     for event in pygame.event.get():
@@ -17,5 +19,8 @@ while True:
             sys.exit()
 
     screen.fill(dark_blue)
+    game_grid.draw(screen)
+        
     pygame.display.update()
     clock.tick(60)
+    
